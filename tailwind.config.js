bfd0d2f76f89126ade1ecc2d554de76a37be5034
Sumukh Bendre <137667597+sumukhbendre123@users.noreply.github.com>
2024-04-07 00:36:56 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: "#5E7BAA",
+        customGray: "#A8C3E8",
+        customDarkBlue: "#0E1623"
+      },
+    },
+  },
+  plugins: [],
+};
